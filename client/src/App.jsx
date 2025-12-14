7738ab7c5b4231bc6a7c2828b41126a1049cf1ba
nima afsharfar <46896398+ne3mer@users.noreply.github.com>
2025-12-14 03:11:48 +0100

    feat(client): Wrap App in AuthProvider and add Auth Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { AuthProvider } from './context/AuthContext';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 const queryClient = new QueryClient();
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </AuthProvider>
     </QueryClientProvider>
   );
 };
